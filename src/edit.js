74c03f953040e6d6a38c36ef3780e76a63debbaf
Nathan Sodja <sodjanathan@gmail.com>
2020-12-25 14:47:10 +0000

    Data Collection Objct is functional

--- src/edit.js
+++ src/edit.js
@@ -1,5 +1,47 @@
-const edit = () => {
-    console.log('edit.js')
+import uuidv4 from "uuid/v4" 
+import Recipe_ from "./recipe"
+
+const dataObject  = {
+    'id' : uuidv4(),
+    'title' : '',
+    'steps' : '',
+    'ingredients' : []
 }
 
-export { edit }
+document.querySelector('#edit-text__title').addEventListener('input',  (e) => {
+
+    dataObject.title = e.target.value
+})
+
+document.querySelector('#edit-text__steps').addEventListener('input',  (e) => {
+    
+    dataObject.steps = e.target.value
+})
+
+document.querySelector('#add-ingredient').addEventListener('change', (e) => {
+
+    dataObject.ingredients.push({
+       'item' : e.target.value,
+       'avail' : false
+    })
+})
+
+document.querySelector('#submit').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(dataObject)
+})
+
+
+
+
+
+
+// console.log(getInput)
+
+// let addRecipe = theRecipe.saveRecipe()
+// addRecipe()
+// console.log(loaded)
+
+// theRecipe.deleteRecipe('93725e56-26ed-47c2-a357-d522304f628b')
+
+// theRecipe.addRecipe()
