12d72fc7798d148603d9639814290eeeb101fe27
Nathan Sodja <sodjanathan@gmail.com>
2020-12-26 14:49:58 +0000

    Search Functions added

---
+++ src/views.js
@@ -0,0 +1,23 @@
+import Recipe_ from "./recipe"
+import { getFilters } from "./filters"
+
+const xRecipes = new Recipe_()
+const recipes = xRecipes.exposeRecipes()
+console.log(recipes)
+
+//Generate RecipeDOM
+const generateRecipeDOM = (recipes) => {
+    const containerElement = document.createElement('div')
+    const recipeElement = document.createElement('a')
+    const checkbox = document.createElement('checkbox')
+    const delButton = document.createElement('button')
+    const recipeText = docment.createElement('h2')
+
+
+}
+
+//Generate ingredients summaryDOM
+
+//Generate ingredientsDDOM
+
+//Generate RecipeTextDOM
